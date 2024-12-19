27c4877627eb14f335e52933d703e62af3128364
ell-bee <l.betancourt25@avenues.org>
2024-12-18 20:22:55 -0500

    rewrote code to incorporate a list
    set background color, loop, random color picking and a background color

--- sketch.js
+++ sketch.js
@@ -1,1 +1,24 @@
+let circles = [];
 
+
+function setup() {
+  createCanvas(400, 400);
+  for (let i = 0; i < 10; i++) {
+    circles.push({
+      x: random(width),
+      y: random(height),
+      size: random(20, 50),
+      color: [random(255), random(255), random(255)],
+    });
+  }
+}
+  
+function draw() {
+  background(255, 204, 215);
+  
+   for (let circle of circles) {
+    fill(circle.color);
+    noStroke();
+    ellipse(circle.x, circle.y, circle.size);
+  }
+}
